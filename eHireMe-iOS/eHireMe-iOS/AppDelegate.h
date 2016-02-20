c1be85ff151f6e63ec84ff5820ff72ec372bed0e
mliu <mliu@iastate.edu>
2016-02-19 19:13:23 -0600

    added IOS Project

---
+++ eHireMe-iOS/eHireMe-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  eHireMe-iOS
+//
+//  Created by Mac Liu on 2/19/16.
+//  Copyright © 2016 eHireMe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ eHireMe-iOS/eHireMe-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  eHireMe-iOS
+//
+//  Created by Mac Liu on 2/19/16.
+//  Copyright © 2016 eHireMe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
