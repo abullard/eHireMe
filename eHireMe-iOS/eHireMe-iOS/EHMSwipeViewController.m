59f265360a32ae48e2700c4146531275d48d5d01
mliu <mliu@iastate.edu>
2016-02-20 21:30:09 -0600

    login functionality completed and updated for iOS and Node.js

---
+++ eHireMe-iOS/eHireMe-iOS/EHMSwipeViewController.m
@@ -0,0 +1,13 @@
+//
+//  EHMSwipeViewController.m
+//  eHireMe-iOS
+//
+//  Created by Mac Liu on 2/20/16.
+//  Copyright © 2016 eHireMe. All rights reserved.
+//
+
+#import "EHMSwipeViewController.h"
+
+@implementation EHMSwipeViewController
+
+@end
