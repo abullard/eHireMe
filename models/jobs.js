03e0b480c2440617d2d2a38c94f3cdbd6c33e39a
mliu <mliu@iastate.edu>
2016-03-01 14:16:48 -0600

    bettered it

---
+++ models/jobs.js
@@ -0,0 +1,36 @@
+/*
+ * Jobs Model.
+ * @author - Mac Liu
+ */
+
+var mongoose = require('mongoose');
+
+//Job Schema
+var JobSchema = new mongoose.Schema({
+	company_name : {
+		type : String,
+		index : true
+	},
+	employer_id : {
+		type : String
+	},
+	description : {
+		type : String
+	},
+	type : {
+		type : String
+	},
+	hourly_rate : {
+		type : String
+	}
+});
+
+var Job = module.exports = mongoose.model('job', JobSchema);
+
+
+/*
+ *	Function creates a new job
+ */
+module.exports.createJob = function(newJob, callback) {
+	newJob.save(callback);
+}
